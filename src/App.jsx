de50a17a220826e2768b867a1f016ad106a45da6
STRANGENIKHIL <nikhilstrange4@gmail.com>
2026-05-29 09:58:52 +0530

    nothing special

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-import React from "react"
+import React, { useState } from "react"
 import Navbar from "./components/Navbar"
+import { Route, Routes ,useLocation } from "react-router-dom"
+import CarDetails from "./pages/CarDetails";
+import Home from "./pages/Home";
+import MyBookings from "./pages/MyBookings";
+import Cars from "./pages/Cars";
+
 const App = () => {
-  return( 
+  const [showLogin , setShowLogin] = useState(false);
+  const isOwnerPath = useLocation().pathname.startsWith('/owner');
+
+  return (
     <div>
-      <Navbar/>
+        {!isOwnerPath && <Navbar setShowLogin={setShowLogin}/>}
+
+
+        <Routes>
+          <Route path='/' element ={<Home/>}/>
+          <Route path='/car-details/:id' element ={<CarDetails/>}/>
+          <Route path='/my-bookings' element ={<MyBookings/>}/>
+          <Route path='/cars' element ={<Cars/>}/>
+        </Routes>
+
+
+
+
     </div>
-    );
+  );
 };
 
 export default App;
