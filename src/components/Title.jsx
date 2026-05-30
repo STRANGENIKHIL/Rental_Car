bfafca9750eac2cea4b618aeedb22e5f3dd123ff
STRANGENIKHIL <nikhilstrange4@gmail.com>
2026-05-30 06:17:49 +0530

    added nothing

---
+++ src/components/Title.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Title = () => {
+  return (
+    <div className={`flex flex-col justify-center items-center text-center`}>
+
+    </div>
+  )
+}
+
+export default Title
