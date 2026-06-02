bf21b6700c9c22568048f43e1c49d02ad9a7baee
STRANGENIKHIL <nikhilstrange4@gmail.com>
2026-06-02 06:46:22 +0530

    feat: implement owner dashboard layout, navigation components, and protected routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ import Home from "./pages/Home";
 import MyBookings from "./pages/MyBookings";
 import Cars from "./pages/Cars";
 import Footer from "./components/Footer";
+import Layout from "./pages/owner/Layout";
+import Dashboard from "./pages/owner/Dashboard";
+import AddCar from "./pages/owner/AddCar";
+import ManageCars from "./pages/owner/ManageCars";
+import ManageBookings from "./pages/owner/ManageBookings";
 
 const App = () => {
   // eslint-disable-next-line no-unused-vars
@@ -22,6 +27,13 @@ const App = () => {
           <Route path='/car-details/:id' element ={<CarDetails/>}/>
           <Route path='/my-bookings' element ={<MyBookings/>}/>
           <Route path='/cars' element ={<Cars/>}/>
+
+          {/*Owner routes */}
+          <Route path='/owner' element={<Layout/>}/>
+          <Route path='/owner/dashboard' element={<Dashboard/>}/>
+          <Route path='/owner/add-car' element={<AddCar/>}/>
+          <Route path='/owner/manage-cars' element={<ManageCars/>}/>
+          <Route path='/owner/manage-bookings' element={<ManageBookings/>}/>
         </Routes>
 
         {/* Global Footer rendered on all consumer-facing pages */}
